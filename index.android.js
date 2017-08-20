ccabd8545f173138367b48472e8e46853b1c444f
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-08-20 11:47:03 +0300

    initial commit

---
+++ index.android.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { 
+  Container, 
+  Header, 
+  Title, 
+  Content, 
+  Footer, 
+  FooterTab, 
+  Button, 
+  Left, 
+  Right, 
+  Body, 
+  Icon, 
+  Text 
+} from 'native-base';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+
+export default class ToDo extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+AppRegistry.registerComponent('ToDo', () => ToDo);
